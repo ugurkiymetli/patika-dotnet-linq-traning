f25f210e846c4bfb04ef0f944862f046396b2142
ugurkiymetli <34272634+ugurkiymetli@users.noreply.github.com>
2021-11-23 15:45:41 +0300

    this is linq practice repo.

---
+++ LinqPractices/DbOperations/LinqDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LinqPractices
+{
+    internal class LinqDbContext : DbContext
+    {
+
+    }
+}
---
+++ LinqPractices/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqPractices
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int ClassId { get; set; }
+    }
+}
